769a3fe812612c2b2fc26de45727744c47061902
Hackett <1017207528@qq.com>
2020-04-29 15:30:52 +0800

    master

---
+++ 02-controls/0-BasicModule.py
@@ -0,0 +1,22 @@
+'''
+作为模板存在的文件
+每次新建文件时可直接利用模板的代码，替换掉FileName部分即可
+节约时间
+'''
+import sys 
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class FileName(QWidget):
+    def __init__(self):
+        super(FileName, self).__init__()
+        self.initUI()
+    def initUI(self):
+        self.setWindowTitle("FileName")
+
+if __name__ == "__main__":
+    app = QApplication()
+    main = FileName()
+    main.show()
+    sys.exit(app.exec_())
--- 02-controls/14-QRadioButtonDemo.py
+++ 02-controls/14-QRadioButtonDemo.py
@@ -6,7 +6,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
-class QRadioButtonDemo(QtWidgets):
+class QRadioButtonDemo(QWidget):
     def __init__(self):
         super(QRadioButtonDemo, self).__init__()
         self.initUI()
@@ -15,10 +15,24 @@ class QRadioButtonDemo(QtWidgets):
         self.setWindowTitle("QRadioButton")
         layout = QHBoxLayout()
         self.button1 = QRadioButton("单选按钮1")
-        # 默认状态
+        # 默认状态，True为默认勾选
         self.button1.setChecked(True)
-        
+        # toggled：状态
+        self.button1.toggled.connect(self.buttonState)
+        layout.addWidget(self.button1)
+        self.button2 = QRadioButton("单选按钮2")
+        self.button2.toggled.connect(self.buttonState)
+        layout.addWidget(self.button2)
+        self.setLayout(layout)
 
+    def buttonState(self):
+        # 用sender接收信号函数传来的数据
+        radioButton = self.sender()
+        if radioButton.isChecked() is True:
+            print(f"< {radioButton.text()} > 被选中")
+        else:
+            print(f"< {radioButton.text()} > 被取消选中状态")
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main = QRadioButtonDemo()
