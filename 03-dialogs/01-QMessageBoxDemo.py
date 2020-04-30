670c37589bdcdbeee4d5b1d180db1391e279c3d6
Hackett <1017207528@qq.com>
2020-04-30 13:13:18 +0800

    master

--- 03-dialogs/01-QMessageBoxDemo.py
+++ 03-dialogs/01-QMessageBoxDemo.py
@@ -4,8 +4,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-class QMessageBoxDemo(QMainWindow
-):
+class QMessageBoxDemo(QWidget):
     def __init__(self):
         super(QMessageBoxDemo, self).__init__()
         self.initUI()
@@ -14,12 +13,22 @@ class QMessageBoxDemo(QMainWindow
         self.setWindowTitle("QMessageBox案例")
         self.resize(300, 400)
         layout = QVBoxLayout()
+        # 要加self，否则无法实现功能
         self.button1 = QPushButton(self)
         self.button1.setText("显示关于对话框")
         self.button1.clicked.connect(self.showDialog)
+        layout.addWidget(self.button1)
+        
+        self.button2 = QPushButton(self)
+        self.button2.setText("显示消息对话框")
+
         self.setLayout(layout)
 
-    def 
+    def showDialog(self):
+        # 千万别忘记sender()这个接受信号数据的方法！
+        text = self.sender().text()
+        if text == "显示关于对话框":
+            QMessageBox.about(self, "关于", "这是一个关于对话框")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
